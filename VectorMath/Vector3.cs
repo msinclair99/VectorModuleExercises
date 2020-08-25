802200f7ee86cae1598e732b28205620f5f4e158
user <user@THE-BEAST>
2020-08-24 19:32:48 -0500

    Finished Creating Vector Math Library

---
+++ VectorMath/Vector3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorLibrary
+{
+    public struct Vector3
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Vector3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+           
+        }
+    }
+}
